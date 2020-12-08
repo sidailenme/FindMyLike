145508b8fb0017d3b623e7a34bb88770c358b273
sidailenme <sidailen.me@gmail.com>
2020-12-08 22:17:54 +0300

    Initial commit

---
+++ src/main/java/com/yalta/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.yalta.controllers;
+
+import com.yalta.services.impl.AuthVksServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthVksServiceImpl authVksService;
+
+    @GetMapping("/get")
+    public String go() {
+        return "redirect:" + authVksService.getAuthURL();
+    }
+
+
+    @GetMapping("/")
+    public void authVk(@RequestParam(value = "code", required = false) String code) {
+        System.out.println(code);
+        authVksService.auth(code);
+    }
+
+
+
+}
---
+++ src/main/java/com/yalta/services/impl/AuthVksServiceImpl.java
@@ -0,0 +1,47 @@
+package com.yalta.services.impl;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Data
+@Service
+@ConfigurationProperties(prefix="spring.security.vk-app")
+public class AuthVksServiceImpl {
+
+    private String clientId;
+    private String clientSecret;
+    private String responseType;
+    private String redirectURL;
+    private String authURL;
+    private String tokenURL;
+    private String display;
+    private String version;
+
+    public String getAuthURL() {
+        return authURL + "?"
+                + "client_id=" + clientId
+                + "&redirect_uri=" + redirectURL
+                + "&display=" + display
+                + "&response_type=" + responseType
+                + "&v=" + version;
+    }
+
+    public String getAccessToken(String code) {
+        return tokenURL + "?"
+                + "client_id=" + clientId
+                + "&client_secret=" + clientSecret
+                + "&redirect_uri=" + redirectURL
+                + "&code=" + code;
+    }
+
+    public void auth(String code) {
+        String responce = getAccessToken(code);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(responce, String.class);
+        System.out.println(responseEntity.toString());
+    }
+
+}
